e0eb4073170cd063dcd9f4c2d4d41865f0c7db8a
helloworld22-joshua <joshua.albrecht@sap.com>
2025-12-29 17:28:40 +0100

    Fixed playlists not showing

--- src/models/spotify.rs
+++ src/models/spotify.rs
@@ -59,26 +59,105 @@ pub struct ExternalUrls {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(default)]
 pub struct Playlist {
     pub id: String,
     pub name: String,
     pub description: Option<String>,
+    #[serde(deserialize_with = "deserialize_null_default")]
     pub images: Vec<Image>,
     pub tracks: PlaylistTracks,
     pub external_urls: ExternalUrls,
     pub owner: PlaylistOwner,
     pub public: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub collaborative: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub href: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub primary_color: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub snapshot_id: Option<String>,
+    #[serde(rename = "type", skip_serializing_if = "Option::is_none")]
+    pub playlist_type: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub uri: Option<String>,
+}
+
+// Custom deserializer to convert null to empty Vec
+fn deserialize_null_default<'de, D, T>(deserializer: D) -> Result<Vec<T>, D::Error>
+where
+    D: serde::Deserializer<'de>,
+    T: serde::Deserialize<'de>,
+{
+    let opt = Option::deserialize(deserializer)?;
+    Ok(opt.unwrap_or_default())
+}
+
+impl Default for Playlist {
+    fn default() -> Self {
+        Self {
+            id: String::new(),
+            name: String::new(),
+            description: None,
+            images: Vec::new(),
+            tracks: PlaylistTracks::default(),
+            external_urls: ExternalUrls { spotify: String::new() },
+            owner: PlaylistOwner::default(),
+            public: None,
+            collaborative: None,
+            href: None,
+            primary_color: None,
+            snapshot_id: None,
+            playlist_type: None,
+            uri: None,
+        }
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct PlaylistTracks {
+    #[serde(default)]
     pub total: u32,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub href: Option<String>,
+}
+
+impl Default for PlaylistTracks {
+    fn default() -> Self {
+        Self {
+            total: 0,
+            href: None,
+        }
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(default)]
 pub struct PlaylistOwner {
     pub display_name: Option<String>,
     pub id: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub external_urls: Option<ExternalUrls>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub href: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub uri: Option<String>,
+    #[serde(rename = "type", skip_serializing_if = "Option::is_none")]
+    pub owner_type: Option<String>,
+}
+
+impl Default for PlaylistOwner {
+    fn default() -> Self {
+        Self {
+            display_name: None,
+            id: String::new(),
+            external_urls: None,
+            href: None,
+            uri: None,
+            owner_type: None,
+        }
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
